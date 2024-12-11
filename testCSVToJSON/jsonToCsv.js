d3f1d2b1d19c8e13c9850cb461e3f0e5cc9b1756
Sandeep Dhanwate <sandydan.dev@gmail.com>
2024-12-11 15:16:31 +0530

    conversion of json to csv file

--- testCSVToJSON/jsonToCsv.js
+++ testCSVToJSON/jsonToCsv.js
@@ -1,25 +1,36 @@
-const fs = require("fs");
-const path = require("path");
-const { Parser } = require("json2csv");
+const fs = require("fs"); // # file system : for handling file operation
+const path = require("path"); // # working with file paths
+const { Parser } = require("json2csv"); // a library for converting json to csv
 
 // read json file path
+// # create a file path for the json file to be read
 const jsonFilePath = path.join(__dirname, "data.json");
 
 try {
-  const jsonData = fs.readFileSync(jsonFilePath, "utf-8"); // string data
+  //
+  const jsonData = fs.readFileSync(jsonFilePath, "utf-8"); // read file as string data
+
   // console.log(jsonData);
-  const data = JSON.parse(jsonData); //
+  const data = JSON.parse(jsonData); // converts the string data into a js object
   //   console.log("Data : ", data);
 
   // create new csv parser
-  const json2CsvParser = new Parser();
-  const csv = json2CsvParser.parse(data);
+  const json2CsvParser = new Parser(); // # creating new csv parser
+  const csv = json2CsvParser.parse(data); // # convert json data to csv format
 
   // write the csv data to file
-  const csvFilePath = path.join(__dirname, "convertedData.csv");
-  fs.writeFileSync(csvFilePath, csv);
+  const csvFilePath = path.join(__dirname, "convertedData.csv"); //# write csv data to a new file
+  fs.writeFileSync(csvFilePath, csv); //# creating csv data
 
   console.log("conversion successfull");
 } catch (error) {
+  // # handle error during above process in this catch block
   console.error("Error while reading or creating json file");
+  if (error.code === "ENOENT") {
+    console.error("File not found or cannot be read.");
+  } else if (error instanceof SyntaxError) {
+    console.error("Invalid JSON format.");
+  } else {
+    console.error(`Unexpected error: ${error.message}`);
+  }
 }
